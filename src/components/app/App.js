f008ded4ff6efcea57381e65125db14a2afe101b
ICELAND\broughw <william.broughton@iceland.co.uk>
2023-08-07 14:59:53 +0100

    Testing fetching products as search result

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [productList, setProductList] = useState([]);
 
-  async function fetchFood() {
-    const response = await fetch(' https://world.openfoodfacts.org/api/v2/product/04963406.json'); // fetching a product by bar code
-    const data = await response.json();
-    console.log(data);
-  }
+  useEffect(() => {
+    fetch('https://us.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=breakfast_cereals&json=true', {
+      'User-Agent': 'HealthyFoodChoices - Android - Version 1.0'
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProductList(data.products);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching product data:', error);
+        setProductList([]);
+      });
+  }, []);
 
-  fetchFood();
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Welcome to our food allergy site!
-        </p>
-        
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>API Test</h1>
+      {productList.map((product) => (
+        <div key={product.code} style={{ margin: '50px auto' }}>
+          <h3>{product.product_name}</h3>
+          <img src={product.image_front_small_url} alt="Product" />
+          <div style={{ margin: '0 30%', fontSize: 12 }}>
+            <p><strong>Allergens:</strong> {product.allergens}</p>
+            <p><strong>Ingredients:</strong> {product.ingredients_text}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
+
 }
 
 export default App;
