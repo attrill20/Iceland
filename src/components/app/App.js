d8f1d4d11a8646bd47735c3c104410c2bde2bcfc
trisrosco <trisrosco@gmail.com>
2023-08-07 13:35:40 +0100

    modified styuling of card, now fetching from local database

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Card from '../card';
+import { foodData } from './foodData';
 
 function App() {
 
@@ -20,8 +21,10 @@ function App() {
         <p>
           Welcome to our food allergy site!
         </p>
-        <Card productName="Milkshake" allergen1="Dairy" allergen2="Egg" dietPref1="Vegetarian" image="https://images.pexels.com/photos/103566/pexels-photo-103566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <Card productName="Omlette" allergen1="Dairy" allergen2="Egg" dietPref1="Vegetarian" image="https://images.pexels.com/photos/103566/pexels-photo-103566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <Card color="#74B737" productName={foodData[1].productName} allergen1={foodData[1].allergen1} allergen2="Egg" dietPref1="Vegetarian" image={foodData[1].image} />
+        <Card color="#74B737" productName="Omlette" allergen1="Dairy" allergen2="Egg" dietPref1="Vegetarian" image="https://media.istockphoto.com/id/1372112175/photo/omelet-frittata-homemade-omelet-isolated-on-dish-with-white-background.jpg?s=1024x1024&w=is&k=20&c=BQwCAU_BeSMn-QTKkUrsL6EN3rd4k6UezoIH8b4OK6A=" />
+        <Card color="#F4EC70" productName="Burger" allergen1="Dairy" allergen2="Egg"  image="https://images.pexels.com/photos/1633577/pexels-photo-1633577.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <Card color="#B85330" productName="Pizza" allergen1="Dairy" allergen2="Egg" allergen3="Nut" image="https://images.pexels.com/photos/1633577/pexels-photo-1633577.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
         
       </header>
     </div>
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,13 +3,13 @@ import "./styles.css";
 
 function Card(props) {
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: props.color }}>
       <h5 className="card-title">{props.productName}</h5>
       <div className="card-body">
         <img className="card-img" src={props.image} alt="Product" />
         <div className="card-info">
           <div className="card-allergens">
-            <p className="allergy">{props.allergen1}</p>
+            <p className="allergy">{props.allergen1} <img className="icon" src="https://img.icons8.com/?size=512&id=10716&format=png" alt="icon"/></p>
             <p className="allergy">{props.allergen2}</p>
             <p className="allergy">{props.allergen3}</p>
             <p className="allergy">{props.allergen4}</p>
