95d3f7c2a7098fb6d0fe2e156be7a11f3200c86f
trisrosco <trisrosco@gmail.com>
2023-08-07 11:56:18 +0100

    basic fetch

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+
+  async function fetchFood() {
+    const response = await fetch(' https://world.openfoodfacts.org/api/v2/product/04963406.json'); // fetching a product by bar code
+    const data = await response.json();
+    console.log(data);
+  }
+
+  fetchFood();
+  
   return (
     <div className="App">
       <header className="App-header">
