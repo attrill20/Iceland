fb17baee23246deef6766f6331f0738f7d533556
trisrosco <trisrosco@gmail.com>
2023-08-07 13:07:26 +0100

    basic styling with dummy data

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Card from '../card';
 
 function App() {
 
 
   async function fetchFood() {
-    const response = await fetch(' https://world.openfoodfacts.org/api/v2/product/04963406.json'); // fetching a product by bar code
+    const response = await fetch('https://world.openfoodfacts.org?json=true&count=50'); // fetching a product by bar code
     const data = await response.json();
     console.log(data);
   }
@@ -19,6 +20,8 @@ function App() {
         <p>
           Welcome to our food allergy site!
         </p>
+        <Card productName="Milkshake" allergen1="Dairy" allergen2="Egg" dietPref1="Vegetarian" image="https://images.pexels.com/photos/103566/pexels-photo-103566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <Card productName="Omlette" allergen1="Dairy" allergen2="Egg" dietPref1="Vegetarian" image="https://images.pexels.com/photos/103566/pexels-photo-103566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
         
       </header>
     </div>
---
+++ src/components/card/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <h5 className="card-title">{props.productName}</h5>
+      <div className="card-body">
+        <img className="card-img" src={props.image} alt="Product" />
+        <div className="card-info">
+          <div className="card-allergens">
+            <p className="allergy">{props.allergen1}</p>
+            <p className="allergy">{props.allergen2}</p>
+            <p className="allergy">{props.allergen3}</p>
+            <p className="allergy">{props.allergen4}</p>
+          </div>
+          <div className="card-dietPrefs">
+            <p className="dietPref">{props.dietPref1}</p>
+            <p className="dietPref">{props.dietPref2}</p>
+            <p className="dietPref">{props.dietPref3}</p>
+            <p className="dietPref">{props.dietPref4}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
