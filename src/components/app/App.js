390abfe482dadd1bd79b6f72a1e9fabcc37a8c27
James Attrill <attrill20@gmail.com>
2023-08-07 10:32:41 +0100

    git init

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Welcome to our food allergy site!
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
